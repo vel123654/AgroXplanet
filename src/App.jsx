25b563e879b0d75eaddcf0a762359a6002837a6c
Nittinthecoder <93333941+Nittinthecoder@users.noreply.github.com>
2024-02-19 11:25:01 +0530

    app.jsx new div created

--- src/App.jsx
+++ src/App.jsx
@@ -9,17 +9,8 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1> hi </h1>
       </div>
-      <h1>Vite + React</h1>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
